95ceed86b1b957175757188bec7b0ab8bea6f8d2
Williams V <wibrvallejoma@gmail.com>
2021-10-02 12:11:03 -0500

    feat: add categories module

---
+++ src/controllers/categories.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('categories')
+export class CategoriesController {
+  @Get(':id/products/:productId')
+  getCategory(@Param('id') id: string, @Param('productId') productId: string) {
+    return `product ${id} and ${productId}`;
+  }
+}
