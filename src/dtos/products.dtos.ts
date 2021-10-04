cdb00800ec02bb4fa2162b14583cbc7c68972fdf
Williams V <wibrvallejoma@gmail.com>
2021-10-03 23:39:39 -0500

    feat: add class validators & mapped types

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,16 +1,34 @@
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  // To add optional variables, just add ? symbol after the variable name.
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
